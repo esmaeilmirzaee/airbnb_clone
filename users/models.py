f265790a29c94d948a1ea8eda6adbd843f7419ab
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-16 23:30:26 +0430

    Improve user db model

--- users/models.py
+++ users/models.py
@@ -14,7 +14,19 @@ class User(AbstractUser):
         (RATHER_GENDER, 'Rather Not Say'),
     )
 
-    avatar = models.ImageField(null=True)
-    bio = models.TextField(default='')
-    gender = models.CharField(max_length=10, null=True, choices=GENDER_CHOICES)
+    CURRENCY_USA = 'usd'
+    CURRENCY_PERSIAN = 'irl'
 
+    CURRENCY_CHOICES = ((CURRENCY_USA, 'USD'), (CURRENCY_PERSIAN, 'IRL'))
+
+    LANGUAGE_ENGLISH = 'en'
+    LANGUAGE_PERSIAN = 'fa'
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, 'en'), (LANGUAGE_PERSIAN, 'fa'))
+
+    avatar = models.ImageField(null=True, required=False)
+    bio = models.TextField(default='', null=True)
+    gender = models.CharField(max_length=14, null=True, choices=GENDER_CHOICES, default=RATHER_GENDER)
+    birthday = models.DateField(null=True, required=False)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=CURRENCY_USA)
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default=LANGUAGE_ENGLISH)
