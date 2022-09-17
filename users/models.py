01dc064f31a62c8c705ef55a342da7f663fb3d2e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-17 13:04:08 +0430

    User admin

--- users/models.py
+++ users/models.py
@@ -25,9 +25,9 @@ class User(AbstractUser):
 
     LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, 'en'), (LANGUAGE_PERSIAN, 'fa'))
 
-    avatar = models.ImageField(null=True, blank=True, default='avatar.png', upload_to='user_avatars')
-    bio = models.TextField(default='', null=True, blank=True)
-    gender = models.CharField(max_length=14, default=RATHER_GENDER, blank=True, null=True, choices=GENDER_CHOICES)
+    avatar = models.ImageField(blank=True, default='avatar.png', upload_to='user_avatars')
+    bio = models.TextField(blank=True)
+    gender = models.CharField(max_length=14, default=RATHER_GENDER, blank=True, choices=GENDER_CHOICES)
     birthdate = models.DateField(null=True, blank=True)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=CURRENCY_USA)
     language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default=LANGUAGE_ENGLISH)
