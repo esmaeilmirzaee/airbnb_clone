0ded9a5a827ad6c04c882ecd6d59d2a70e16d31b
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-17 12:44:00 +0430

    A full custom user page

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomAdminUser(admin.ModelAdmin):
+class CustomAdminUser(UserAdmin):
     list_display = ('username', 'superhost', 'language', 'currency',)
     list_filter = ('superhost',)
+
+    fieldsets = UserAdmin.fieldsets + (
+            ("Change portfolio", {
+                    "fields": ('avatar', 'gender', 'birthdate', 'bio', 'currency', 'language')
+            }),
+    )
 
 
 # admin.site.register(models.User, CustomAdminUser)
