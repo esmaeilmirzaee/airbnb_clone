7de3bc17b6916d4764b4ee9d0fb0dcde96481c54
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-17 22:28:59 +0430

    Type of home

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.TypeOfRoom)
+class TypeOfRoomAdmin(admin.ModelAdmin):
+    ...
+
+
 class RoomsAdmin(admin.ModelAdmin):
     pass
 
--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,22 @@ from core import models as core_model
 from users import models as user_model
 
 
+class AbstractItem(core_model.TimeStampedModel):
+
+    name = models.CharField(max_length=90)
+    description = models.CharField(max_length=130, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class TypeOfRoom(AbstractItem):
+    pass
+
+
 class Room(core_model.TimeStampedModel):
     """Room Model"""
 
@@ -21,6 +37,7 @@ class Room(core_model.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_model.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(TypeOfRoom, blank=True)
 
     def __str__(self):
         return self.name
