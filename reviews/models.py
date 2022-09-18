e0741401b5f7ad10164044e99ae924466c3f682c
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-18 11:49:18 +0430

    Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class Review(TimeStampedModel):
+    content = models.TextField()
+    accuracy = models.DecimalField()
+    cleanliness = models.DecimalField()
+    communication = models.DecimalField()
+    location = models.DecimalField()
+    check_in = models.DecimalField()
+    value = models.DecimalField()
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    room = models.ForeignKey('rooms.Room', on_delete=models.CASCADE)
