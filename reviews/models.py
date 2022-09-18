c8a26bf6d8c2c42ae2892011ac0a0d0e23a16d9c
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-18 12:17:18 +0430

    Review model

--- reviews/models.py
+++ reviews/models.py
@@ -5,11 +5,14 @@ from core.models import TimeStampedModel
 
 class Review(TimeStampedModel):
     content = models.TextField()
-    accuracy = models.DecimalField()
-    cleanliness = models.DecimalField()
-    communication = models.DecimalField()
-    location = models.DecimalField()
-    check_in = models.DecimalField()
-    value = models.DecimalField()
+    accuracy = models.DecimalField(decimal_places=1, max_digits=2)
+    cleanliness = models.DecimalField(decimal_places=1, max_digits=2)
+    communication = models.DecimalField(decimal_places=1, max_digits=2)
+    location = models.DecimalField(decimal_places=1, max_digits=2)
+    check_in = models.DecimalField(decimal_places=1, max_digits=2)
+    value = models.DecimalField(decimal_places=1, max_digits=2)
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     room = models.ForeignKey('rooms.Room', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.room} by {self.user.username.upper()}'
