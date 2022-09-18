df35567b80b15dbe18e0fede960c4c0b5cb32122
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-18 15:10:33 +0430

    Admin panel for room

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from . import models
 
 
@@ -8,13 +9,50 @@ class TypeOfRoomAdmin(admin.ModelAdmin):
 
 
 class RoomsAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'name',
+        'room_type',
+        'city',
+        'country',
+        'price',
+        'instant_book'
+    )
+    fieldsets = (
+        (
+            'Basic Info',
+            {'fields': ('name', 'country', 'city', 'price',)}
+        ),
+        (
+            'Spaces',
+            {'fields': ('baths', 'beds', 'bedrooms')}
+        ),
+        (
+            'Date and Time',
+            {
+                'fields': ('check_in', 'check_out', 'instant_book',)
+            }
+        ),
+        (
+            'Owner',
+            {'fields': ('host',)}
+        ),
+        (
+            'More About the Spaces',
+            {
+                'classes': ('collapse',),
+                'fields': ('amenities', 'facilities', 'house_rules',),
+            }
+        ),
+    )
+
+    list_filter = ('country', 'city',)
+    search_fields = ('city', '=name', '^host__username')
+    filter_horizontal = ("amenities", 'facilities', 'house_rules',)  # By default, just many to many
 
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
     ...
-
 
 
 admin.site.register(models.Room, RoomsAdmin)
