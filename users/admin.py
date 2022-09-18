464571dc395f283d2653fc9318283f1ed0dd45e8
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-18 18:20:47 +0430

    User admin model

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from . import models
 
 @admin.register(models.User)
 class CustomAdminUser(UserAdmin):
-    list_display = ('username', 'superhost', 'language', 'currency',)
+    list_display = ('username', 'superhost', 'get_language', 'currency', 'is_active', 'is_staff', 'is_superuser',)
     list_filter = ('superhost',)
 
     fieldsets = UserAdmin.fieldsets + (
@@ -13,6 +13,14 @@ class CustomAdminUser(UserAdmin):
                     "fields": ('avatar', 'gender', 'birthdate', 'bio', 'currency', 'language')
             }),
     )
+
+    list_filter = UserAdmin.list_filter + (
+        'superhost',
+        'language',
+    )
+
+    def get_language(self, obj):
+        return obj.language.upper()
 
 
 # admin.site.register(models.User, CustomAdminUser)
