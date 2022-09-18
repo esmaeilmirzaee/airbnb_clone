be38aed3fdab907a64c50d34d6f2ba55076187f9
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-18 22:35:32 +0430

    Talk and participant admin portal

--- talks/admin.py
+++ talks/admin.py
@@ -4,11 +4,11 @@ from . import models
 
 @admin.register(models.Talk)
 class TalksAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('__str__', 'created_at',)
 
 
 class ParticipantsAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('__str__', 'number_of_messages', 'number_of_participants',)
 
 
 admin.site.register(models.Participants, ParticipantsAdmin)
--- talks/models.py
+++ talks/models.py
@@ -4,15 +4,25 @@ from core.models import TimeStampedModel
 
 class Talk(TimeStampedModel):
     message = models.TextField()
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    participants = models.ForeignKey('Participants', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', related_name='talk', on_delete=models.CASCADE)
+    participants = models.ForeignKey('Participants', related_name='talk', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user}: {self.message[:12]}'
 
 
 class Participants(TimeStampedModel):
-    actor = models.ManyToManyField('users.User')
+    actor = models.ManyToManyField('users.User', related_name='participant')
 
     def __str__(self):
-        return f'{self.actor.count()} @ {self.created_at.date()}'
+        usernames = []
+        for user in self.actor.all():
+            usernames.append(user.username)
+
+        return f'{", ".join(usernames)} @{self.created_at.date()} [{self.actor.count()}]'
+
+    def number_of_messages(self):
+        return self.talk.count()
+
+    def number_of_participants(self):
+        return self.actor.count()
